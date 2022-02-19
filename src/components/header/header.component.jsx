040811fb7ccf8ff3691a5fd8ab5c557d72e21a0d
shsv382 <shsv382@gmail.com>
2022-02-19 11:39:52 +0300

    Initial commit, added fonts, installed SASS

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './header.styles.scss';
+
+import HeaderContainer from "../header-container/header-container.component";
+
+const Header = () => {
+    return (
+        <HeaderContainer />
+    )
+}
+
+export default Header;
