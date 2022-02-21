76ba7a9f16b2cb9f80990f399090d4912a71bf4f
shsv382 <shsv382@gmail.com>
2022-02-21 13:37:36 +0300

    Added contact page

--- src/pages/contacts/contact.component.jsx
+++ src/pages/contacts/contact.component.jsx
@@ -3,7 +3,36 @@ import './contact.styles.scss';
 
 const Contact = () => {
     return (
-        <h1>CONTACT</h1> 
+        <div className='contact-page'>
+            <h1>Свяжитесь со мной любым удобным для вас способом!</h1>
+            <ul className='messengers'>
+                <li>
+                    <a href="tel:12345678">
+                        <img alt="phone" src='images/contact-phone.png' />
+                    </a>
+                </li>
+                <li>
+                    <a href='mailto:shsv382@yandex.ru'>
+                        <img alt="email" src='images/contact-email.png' />
+                    </a>
+                </li>
+                <li>
+                    <a href='https://wa.me/79180071601'>
+                        <img alt="whatsapp" src='images/contact-whatsapp.png' />
+                    </a>
+                </li>
+                <li>
+                    <a href='https://t.me/sergy_shaby'>
+                        <img alt="telegram" src='images/contact-telegram.png' />
+                    </a>
+                </li>
+                <li>
+                    <a href="https://viber.click/79180071601">
+                        <img alt="viber" src='images/contact-viber.png' />
+                    </a>
+                </li>
+            </ul>
+        </div>
     )
 }
 
